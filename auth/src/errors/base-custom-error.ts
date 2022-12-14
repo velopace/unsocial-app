1ecb679b5db5d664ed9a9d9df3b6bef62a44cc7c
velopace <velopace@outlook.com>
2022-12-14 08:38:00 -0600

    feat(auth): implement unit tests for InvalidInput error

--- auth/src/errors/base-custom-error.ts
+++ auth/src/errors/base-custom-error.ts
@@ -1,6 +1,4 @@
 export default abstract class BaseCustomError extends Error {
-  abstract statusCode: number;
-
   protected constructor(message?: string) {
     super(message);
   }
--- auth/src/errors/invalid-input.ts
+++ auth/src/errors/invalid-input.ts
@@ -1,17 +1,51 @@
+import { ValidationError } from 'express-validator';
 import BaseCustomError from './base-custom-error';
+import {
+  SerializedErrorField,
+  SerializedErrorOutput,
+} from './types/serialized-error-output';
+
+export type InvalidInputConstructorErrorsParam = ValidationError[];
 
 export default class InvalidInput extends BaseCustomError {
-  statusCode = 422;
+  protected statusCode = 422;
 
-  constructor() {
+  protected errors: ValidationError[] | undefined;
+
+  private errorMessage = 'The input provided is invalid';
+
+  constructor(errors?: InvalidInputConstructorErrorsParam) {
     super('The input provided is invalid');
+    this.errors = errors;
   }
 
   getStatusCode(): number {
     return this.statusCode;
   }
 
-  serializeErrorOutput(): unknown {
-    return undefined;
+  serializeErrorOutput(): SerializedErrorOutput {
+    return this.parseValidationErrors();
+  }
+
+  private parseValidationErrors(): SerializedErrorOutput {
+    const parsedErrors: SerializedErrorField = {};
+    if (this.errors && this.errors.length > 0) {
+      this.errors.forEach((error) => {
+        if (parsedErrors[error.param]) {
+          parsedErrors[error.param].push(error.msg);
+        } else {
+          parsedErrors[error.param] = [error.msg];
+        }
+      });
+    }
+
+    return {
+      errors: [
+        {
+          message: this.errorMessage,
+          fields: parsedErrors,
+        },
+      ],
+    };
   }
 }
